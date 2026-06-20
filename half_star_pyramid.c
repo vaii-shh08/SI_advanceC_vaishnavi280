817eb50764a57db6e50391400819c42d749d853b
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-20 23:55:41 +0530

    Pattern of half star pyramid

---
+++ half_star_pyramid.c
@@ -0,0 +1,15 @@
+// Print a half-pyramid of stars 
+#include <stdio.h>
+int main (){
+    int row;
+    printf("Enter the number of rows :");
+    scanf("%d",&row);
+    for(int i = 0; i<row ; i++){
+        for(int j = 0 ; j<=i ; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
