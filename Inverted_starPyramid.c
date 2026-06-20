2fb3d5c1346639cd9f6cf83d355ac2251bb07657
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-20 23:51:48 +0530

    Pattern of inverted half pyramid

---
+++ Inverted_starPyramid.c
@@ -0,0 +1,15 @@
+// Print an inverted half-pyramid
+#include <stdio.h>
+int main (){
+    int row;
+    printf("Enter the number of rows : ");
+    scanf("%d",&row);
+    for(int i = row ; i>=1 ; i--){
+        for(int j = 1 ; j<=i ; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
