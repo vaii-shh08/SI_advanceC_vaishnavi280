847ad2ea7e8f40fd3b0c6a69b0e57686353f2816
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-20 19:39:43 +0530

    Vowels And Consonant

---
+++ vowels_consonant.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    char ch ;
+    printf("Enter the  character :");
+    scanf(" %c" , &ch);
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'|| ch=='A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+        printf("Character is vowel !");
+    }
+    else{
+        printf("Character is consonant!");
+    }
+    return 0;
+}
