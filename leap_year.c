a1a5845754e38a787f836351d1b5e197f3994b29
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-20 19:07:09 +0530

    codefor checking leap year or not

---
+++ leap_year.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    int year;
+    printf("Enter the year : ");
+    scanf("%d", &year);
+    if (year%400==0 ||(year%4==0 && year%100!=0) )
+    printf("Leap year!");
+    else
+    printf("Not a leap year!");
+    return 0;
+}
