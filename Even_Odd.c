049d921dafeeaae27e1468fe48001457041f399e
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-20 19:23:17 +0530

    checking even odd  with if else

---
+++ Even_Odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int num;
+    printf("Enter the number :");
+    scanf("%d",&num);
+    if(num%2 == 0){
+        printf("Number is even!");
+    }
+    else {
+        printf("Number is odd!");
+    }
+    return 0;
+}
