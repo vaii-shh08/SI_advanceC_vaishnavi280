9c2dce7859f217565cf470a3a236067bf56acc01
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-23 11:17:44 +0530

    If a number is even or odd using bitwise operators

---
+++ odd_even_bitwise.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num & 1)
+        printf("%d is Odd", num);
+    else
+        printf("%d is Even", num);
+
+    return 0;
+}
