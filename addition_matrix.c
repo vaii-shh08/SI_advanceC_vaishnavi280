2a510b64f68f9c55dbab1d9598de59898e610440
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-23 09:03:40 +0530

    Addition of two matrices

---
+++ addition_matrix.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main (){
+    int a[3][3] , b[3][3] , c[3][3];
+    printf("Enter the elements of matrix A : \n");
+    for(int i = 0 ; i<3; i++){
+        for(int j = 0 ; j<3; j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+     printf("Enter the elements of matrix B : \n");
+    for(int i = 0 ; i<3; i++){
+        for(int j = 0 ; j<3; j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(int i = 0 ; i<3; i++){
+        for(int j = 0 ; j<3; j++){
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("The Resultant Matrix is : \n");
+    for(int i = 0 ; i<3; i++){
+        for(int j = 0 ; j<3; j++){
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
