3fcabe37b524493b74f226c7b944d75406d4a523
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-23 10:58:08 +0530

    size of different data types using sizeof operator

---
+++ sizeof_dataTypes.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Size of char   = %d byte\n", sizeof(char));
+    printf("Size of int    = %d bytes\n", sizeof(int));
+    printf("Size of float  = %d bytes\n", sizeof(float));
+    printf("Size of double = %ld bytes\n", sizeof(double));
+    return 0;
+}
