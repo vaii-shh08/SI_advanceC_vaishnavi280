f69bed0a02fd9330e6799540b5ab5adcd3d5f904
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-26 23:11:18 +0530

    Copy one string to another without strcpy

---
+++ copy_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char str1[100], str2[100];
+    int i;
+    printf("Enter a string: ");
+    scanf("%s", str1);
+    for(i = 0; str1[i] != '\0'; i++)
+    {
+        str2[i] = str1[i];
+    }
+    str2[i] = '\0';
+    printf("Copied String: %s", str2);
+    return 0;
+}
