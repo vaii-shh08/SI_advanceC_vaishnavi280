d499b417ab0272fa62d6e0def747531c3bff2f5d
Vaishnavi <vaishnavi125rr@gmail.com>
2026-06-27 08:05:39 +0530

    Find the sum of natural numbers using Recursion

---
+++ sum_natural_no.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int sum(int n)
+{
+    if (n == 0)
+        return 0;
+    return n + sum(n - 1);
+}
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Sum of first %d natural numbers = %d", n, sum(n));
+    return 0;
+}
